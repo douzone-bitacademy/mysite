6097303e4bf03db2f27ecefaa26b7dc41988bd95
R35 <R35@DESKTOP-C4F52MO>
2020-02-11 10:33:35 +0900

    init commit

---
+++ mysite02/src/main/java/com/douzone/mysite/controller/GuestbookController.java
@@ -0,0 +1,31 @@
+package com.douzone.mysite.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.douzone.web.util.WebUtil;
+
+public class GuestbookController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String actionName = request.getParameter("a");
+		
+		if("deleteform".equals(actionName)) {
+			
+		} else if("delete".equals(actionName)) {
+			
+		} else {
+			// default 요청 처리 (list)
+			WebUtil.forward("/WEB-INF/views/guestbook/list.jsp", request, response);
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
