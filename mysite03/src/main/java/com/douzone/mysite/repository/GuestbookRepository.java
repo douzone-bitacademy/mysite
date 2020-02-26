6a9d857ed49b2f066272c57e0e0ebf139d5849db
R35 <R35@DESKTOP-C4F52MO>
2020-02-26 12:32:39 +0900

    방명록 구현완료

--- mysite03/src/main/java/com/douzone/mysite/repository/GuestbookRepository.java
+++ mysite03/src/main/java/com/douzone/mysite/repository/GuestbookRepository.java
@@ -1,121 +1,30 @@
 package com.douzone.mysite.repository;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
-import com.douzone.mysite.exception.GuestbookRepositoryException;
 import com.douzone.mysite.vo.GuestbookVo;
 
-
+@Repository
 public class GuestbookRepository {
 	
 	@Autowired
 	private SqlSession sqlSession;
 	
-	public int delete( GuestbookVo vo ) {
-		int count = 0;
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-
-		try {
-			conn = getConnection();
-
-			String sql = 
-				" delete" + 
-				"   from guestbook" + 
-				"  where no=?" +
-				"    and password=?";
-			pstmt = conn.prepareStatement( sql );
-
-			pstmt.setLong( 1, vo.getNo() );
-			pstmt.setString( 2, vo.getPassword() );
-
-			count = pstmt.executeUpdate();
-		} catch (SQLException e) {
-			throw new GuestbookRepositoryException(e.getMessage());
-		} finally {
-			// 자원 정리
-			try {
-				if (pstmt != null) {
-					pstmt.close();
-				}
-				if (conn != null) {
-					conn.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-
-		return count;		
-	}
-	
-	public int insert(GuestbookVo vo) {
-		int count = 0;
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-
-		try {
-			conn = getConnection();
-
-			String sql = 
-				" insert" + 
-				"   into guestbook(name, password, contents, reg_date)" + 
-				" values (?, ?, ?, now() )";
-			pstmt = conn.prepareStatement(sql);
-
-			pstmt.setString(1, vo.getName());
-			pstmt.setString(2, vo.getPassword());
-			pstmt.setString(3, vo.getContents());
-
-			count = pstmt.executeUpdate();
-
-		} catch (SQLException e) {
-			throw new GuestbookRepositoryException(e.getMessage());
-		} finally {
-			// 자원 정리
-			try {
-				if (pstmt != null) {
-					pstmt.close();
-				}
-				if (conn != null) {
-					conn.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-
-		return count;
-	}
-
-	public List<GuestbookVo> getList() {
-		List<GuestbookVo> list = sqlSession.selectList("guestbook.findAll");
+	public List<GuestbookVo> findAll() {
+		List<GuestbookVo> list = sqlSession.selectList( "guestbook.findAll" );
 		return list;
 	}
-
-	private Connection getConnection() throws SQLException {
-		Connection conn = null;
-		try {
-			//1. 드라이버 로딩
-			Class.forName( "com.mysql.jdbc.Driver" );
-			
-			//2. 연결하기
-			String url="jdbc:mysql://localhost/webdb";
-			conn = DriverManager.getConnection(url, "webdb", "webdb");
-		} catch( ClassNotFoundException e ) {
-			throw new GuestbookRepositoryException("드러이버 로딩 실패:" + e);
-		} 
-		
-		return conn;
-	}	
+	
+	public int delete( GuestbookVo vo ) {
+		int count = sqlSession.delete( "guestbook.delete", vo );
+		return count;
+	}
+	
+	public int insert( GuestbookVo vo ) {
+		return sqlSession.insert( "guestbook.insert", vo );
+	}
 }
