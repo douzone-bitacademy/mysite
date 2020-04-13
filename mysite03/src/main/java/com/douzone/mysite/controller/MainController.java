077109dba1903b4d1ab25e4063e0d6ac4b1038f2
Dae H. Ahn <kickscar@gmail.com>
2020-04-13 16:13:14 +0900

    ajax #2 - check email

--- mysite03/src/main/java/com/douzone/mysite/controller/MainController.java
+++ mysite03/src/main/java/com/douzone/mysite/controller/MainController.java
@@ -2,11 +2,20 @@ package com.douzone.mysite.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.douzone.mysite.vo.UserVo;
 
 @Controller
 public class MainController {
 	@RequestMapping({"", "/main"})
 	public String index() {
 		return "main/index";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/hello")
+	public UserVo hello() {
+		return new UserVo();
 	}
 }
